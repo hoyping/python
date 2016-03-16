6cdafba0404d24ff2a6632a41f0de3b1edb56ac1
Roy.Hu <hoyping@163.com>
2016-03-17 00:12:30 +0800

    init

---
+++ confParser.py
@@ -0,0 +1,14 @@
+#######################################
+#File Name: confParser.py
+#Created Time: 2016-03-17 00:10:08
+#######################################
+#!/usr/bin/env python
+
+import ConfigParser
+
+class ConfParser():
+    def __init__(self):
+        pass
+
+    def get_value(self):
+        pass
---
+++ logger.py
@@ -0,0 +1,11 @@
+#######################################
+#File Name: logger.py
+#Created Time: 2016-03-17 00:11:56
+#######################################
+#!/usr/bin/env python
+
+import logging
+
+class Logger():
+    def __init__(self):
+        pass
